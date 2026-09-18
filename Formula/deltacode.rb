d698b03b850606681d291a5bc8772068b705686f
constantine@aitrailblazer.com <constantine@aitrailblazer.com>
2026-09-18 08:56:07 -0700

    release: v1.0.2 (CAS hash anchoring, semantic AST invariance and build-tag hardening)

--- Formula/deltacode.rb
+++ Formula/deltacode.rb
@@ -1,26 +1,26 @@
 class Deltacode < Formula
   desc "Sub-millisecond AST context slicing & token optimization engine for AI coding"
   homepage "https://platform.aitrailblazer.com"
-  version "1.0.1"
+  version "1.0.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/aitrailblazer/homebrew-deltacode/releases/download/v1.0.1/deltacode_darwin_arm64.tar.gz"
-      sha256 "932cbdf265ab8549c0a7d8e8a4be5b4ff3bf99e077cb0b8ce27d1f082fb3a4e4"
+      url "https://github.com/aitrailblazer/homebrew-deltacode/releases/download/v1.0.2/deltacode_darwin_arm64.tar.gz"
+      sha256 "143f9b5241d1c5132cf8a70bcc9e0a7dbafe4d403d9aa00e1c2ec92a92a5a605"
     else
-      url "https://github.com/aitrailblazer/homebrew-deltacode/releases/download/v1.0.1/deltacode_darwin_amd64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/aitrailblazer/homebrew-deltacode/releases/download/v1.0.2/deltacode_darwin_amd64.tar.gz"
+      sha256 "2e8ab9732f9b39de8f078a1f981ba25912b5923cccae7d2d6a1d666b0f98f069"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/aitrailblazer/homebrew-deltacode/releases/download/v1.0.0/deltacode_linux_arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/aitrailblazer/homebrew-deltacode/releases/download/v1.0.2/deltacode_linux_arm64.tar.gz"
+      sha256 "5897bf933f6a777a8fffb34b4adae4ad30263ea4e20f464fde9ec50d5a6e8ce8"
     else
-      url "https://github.com/aitrailblazer/homebrew-deltacode/releases/download/v1.0.0/deltacode_linux_amd64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      url "https://github.com/aitrailblazer/homebrew-deltacode/releases/download/v1.0.2/deltacode_linux_amd64.tar.gz"
+      sha256 "eb55897894e6ed77a2956e807c6b810b4790d832edab9dd8bad5d76052cf2aef"
     end
   end
 
